667c46711e01ca585d8bb27e1f2eade988ebab3c
Sharkzod <godstimeamerica99@gmail.com>
2025-07-23 08:52:09 +0100

    sound

--- src/App.tsx
+++ src/App.tsx
@@ -127,7 +127,7 @@ const Waitlist = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.6 }}
         src="/Group-19.png"
-        className="h-[25vh] w-[23%] top-[75vh] sm:h-[45vh] block absolute bottom-0 sm:w-[14%]  sm:top-[55vh] right-0 mb-4 mr-4 sm:mr-12"
+        className="h-[25vh] w-[38%] top-[75vh] sm:h-[45vh] block absolute bottom-0 sm:w-[14%]  sm:top-[55vh] right-0 mb-4 mr-4 sm:mr-12"
       />
 
       <motion.img
@@ -135,7 +135,7 @@ const Waitlist = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.6 }}
         src="/Layer_1.png"
-        className="h-[25vh] w-[23%] top-[75vh] sm:h-[45vh] block absolute bottom-0 sm:w-[14%]  sm:top-[55vh] left-0 mb-4 ml-4 sm:ml-12"
+        className="h-[25vh] w-[40%] top-[75vh] sm:h-[45vh] block absolute bottom-0 sm:w-[14%]  sm:top-[55vh] left-0 mb-4 ml-4 sm:ml-12"
       />
     </motion.main>
   );
