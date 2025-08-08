dfd379995deb793751ee7716bdca16b41f0524ec
Sharkzod <godstimeamerica99@gmail.com>
2025-08-08 13:56:30 +0100

    mobile

--- src/App.tsx
+++ src/App.tsx
@@ -89,17 +89,17 @@ const Waitlist = () => {
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.9 }}
-        className="flex flex-row sm:flex-row justify-center gap-4 sm:gap-8 mt-2 sm:mt-12 text-center w-full px-4"
+        className=" w-full px-0 flex flex-row sm:flex-row justify-center gap-4 sm:gap-8 mt-2 sm:mt-12 text-center sm:px-4"
       >
-        <div className="px-2 py-2 sm:py-0">
+        <div className="px-0 sm:py-0 sm:px-2 py-2 w-full">
           <div className="text-lg sm:text-xl font-semibold">32M+</div>
           <div className="text-gray-500 text-xs sm:text-sm">Tonnes of waste generated yearly</div>
         </div>
-        <div className="px-2 py-2 sm:py-0">
+        <div className="px-0 sm:py-0 sm:px-2 py-2 w-full">
           <div className="text-lg sm:text-xl font-semibold">₦15K</div>
           <div className="text-gray-500 text-xs sm:text-sm">Average monthly earnings potential</div>
         </div>
-        <div className="px-2 py-2 sm:py-0">
+        <div className="px-0 sm:py-0 sm:px-2 py-2 w-full">
           <div className="text-lg sm:text-xl font-semibold">100%</div>
           <div className="text-gray-500 text-xs sm:text-sm">Environmental impact improvement</div>
         </div>
@@ -122,21 +122,92 @@ const Waitlist = () => {
         className="w-full  absolute bottom-0 h-[35vh] sm:h-[25vh] sm:top-[80vh]"
       />
 
+  
+
+
+      <motion.div>
       <motion.img
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 1.2 }}
+        src="/can.png"
+        className="hidden rotate-3 h-[25vh] w-[40%] top-[95vh] sm:h-[23vh] sm:block absolute bottom-0 sm:w-[6%]  sm:top-[45vh] left-[27px] mb-4 ml-4 sm:ml-12"
+      />
+
+        <motion.img
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 1.0 }}
+        src="/bag.png"
+        className="hidden h-[25vh] w-[40%] top-[95vh] sm:h-[23vh] sm:block absolute bottom-0 sm:w-[9%]  sm:top-[56vh] left-[100px] mb-4 ml-4 sm:ml-12"
+      />
+
+        <motion.img
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.8 }}
+        src="/small-can.png"
+        className="hidden h-[25vh] w-[40%] top-[95vh] sm:h-[11vh] sm:block absolute bottom-0 sm:w-[8%]  sm:top-[67vh] left-0 mb-4 ml-4 sm:ml-12"
+      />
+        <motion.img
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.6 }}
-        src="/Group-19.png"
-        className="h-[25vh] w-[38%] top-[75vh] sm:h-[45vh] block absolute bottom-0 sm:w-[14%]  sm:top-[55vh] right-0 mb-4 mr-4 sm:mr-12"
+        src="/group-25.png"
+        className="hidden h-[25vh] rotate-12 sm:rotate-0 w-[40%] left-0 top-[85vh] sm:h-[20vh] sm:block absolute bottom-0 sm:w-[13%]  sm:top-[80vh] sm:left-8 mb-4 ml-4 sm:ml-12"
+      />
+      </motion.div>
+
+      <motion.div>
+      <motion.img
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 1.2 }}
+        src="/sanitizer.png"
+        className="hidden h-[25vh] w-[40%] top-[95vh] sm:h-[12vh] sm:block absolute bottom-0 sm:w-[5%]  sm:top-[58vh] right-[80px] mb-4 ml-4 sm:ml-12"
+      />
+
+        <motion.img
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 1.0 }}
+        src="/big-can.png"
+        className="hidden h-[25vh] w-[40%] top-[95vh] sm:h-[17vh] sm:block absolute bottom-0 sm:w-[7%]  sm:top-[64vh] right-[130px] mb-4 ml-4 sm:ml-12"
+      />
+
+        <motion.img
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.8 }}
+        src="/little.png"
+        className="hidden h-[25vh] w-[40%] top-[95vh] sm:h-[11vh] sm:block absolute bottom-0 sm:w-[8%]  sm:top-[67vh] right-[30px] mb-4 ml-4 sm:ml-12"
+      />
+        <motion.img
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.6 }}
+        src="/trash.png"
+        className="hidden h-[25vh] w-[40%] top-[95vh] sm:h-[20vh] sm:block absolute bottom-0 sm:w-[13%]  sm:top-[80vh] right-8 mb-4 ml-4 sm:ml-12"
+      />
+      </motion.div>
+<motion.img
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.6 }}
+        src="/Group-19-slant.png"
+        className="absolute right-0 top-[75vh] w-[40%] h-[25vh]"
       />
 
       <motion.img
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.6 }}
-        src="/Layer_1.png"
-        className="h-[25vh] w-[40%] top-[75vh] sm:h-[45vh] block absolute bottom-0 sm:w-[14%]  sm:top-[55vh] left-0 mb-4 ml-4 sm:ml-12"
+        src="/Layer_1-slant.png"
+        className="absolute left-0 top-[75vh] w-[40%] h-[25vh]"
       />
+      <motion.div>
+
+      </motion.div>
     </motion.main>
   );
 };
